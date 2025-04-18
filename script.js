dfecf8ad81d2deaad9df0bd8786fd43eaad20435
Zerho92 <joshua921@hotmail.es>
2025-04-18 12:36:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ document.getElementById('contact-form')?.addEventListener('submit', async functi
     });
 
     if (response.ok) {
-      window.location.href = 'gracias.html';
+      window.location.href = 'https://zerho92.github.io/josh/gracias.html';
     } else {
       throw new Error('Error en el servidor');
     }
